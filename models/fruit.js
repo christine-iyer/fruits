e6353a7f758d3cfb26e1197fc9806547e12faf33
christine iyer <edithbird5@gmail.com>
2022-10-08 06:46:30 -0400

    fruits

---
+++ models/fruit.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose');
+
+//Make a Schema
+const fruitSchema = new mongoose.Schema({
+     name: { type: String, required: true }, 
+     color: { type: String, required: true }, 
+     readyToEat: Boolean
+});
+
+//Make a Model from the Schema
+//the Frut model allows us to use the CRUD Create Read Update Destroy in the app
+
+const Fruit = mongoose.model('Fruit', fruitSchema);
+//making a fruits collection
+
+//Export the Model for Use in the App
+module.exports=Fruit;
