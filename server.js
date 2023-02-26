56422050f31e268be4a4a6910cb5687b96651555
chrisIyer <hellohotbox420@gmail.com>
2023-02-26 16:43:38 -0500

    still unable to resolve this problem.

--- server.js
+++ server.js
@@ -35,7 +35,30 @@ app.use(
     saveUninitialized: true,
     resave: false,
   })
+
+  
 )
+
+const getResults = async () => {
+  try {
+    const response = await fetch('https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=GOOGL%2CAMZN%2CAAPL%2CBA%2CCOIN%2CCVS%2CGS%2CMS%2CNVDA%2CPYPL%2CPFE%2CCRM%2CSBUX%2CTSLA%2CDIS%2CVTI%2CLI', {
+      method: "GET",
+      headers: {
+        'X-RapidAPI-Key': '5e4d0eeb5bmsh1f0574004d6dfb6p160e9fjsnd9a3ae03ad63',
+        'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
+      }
+    })
+    const data = await response.json()
+    setResults(data.quoteResponse.result)
+    console.log(data)
+
+    return data
+  } catch (e) {
+    console.error(e);
+    setErrorMessage(e.message);
+    return e.message
+  }
+}
 
 app.use('/fruits', require('./controllers/routeController'))
 // send all "/fruits" routes to main router (in a more advanced app we could have multiple routers for multiple paths)
@@ -47,9 +70,9 @@ app.get('/', (req, res) => {
 /* END Middleware */
 
 // Tell the app to listen on a port
-app.listen(3000, () => {
-  console.log('Listening on Port 3000')
-})
+// app.listen(3000, () => {
+//   console.log('Listening on Port 3000')
+// })
 // in your code
 const PORT = process.env.PORT || 8000
 
